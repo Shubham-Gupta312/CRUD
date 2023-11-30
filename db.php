82ce630ebf897193926098a97cf8cc7d3225d9d9
Shubham Gupta <71428139+Shubham-Gupta312@users.noreply.github.com>
2023-11-30 18:20:04 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // Replace with your MySQL server name
 $username = "root"; // Replace with your MySQL username
 $password = ""; // Replace with your MySQL password
-$dbname = "crud"; // Replace with your database name
+$dbname = ""; // Replace with your database name
 
 
 // Create connection
